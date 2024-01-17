f6828cbde3bfead756da44fe833fe06a0e6bd342
Braulee7 <braulio00001@gmail.com>
2024-01-16 16:37:26 -0800

    Created perlin noise library

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,43 @@
+/*
 #include "evn_app.h"
 
 int main(void)
 {
 	evn::App app("evn Engine");
 	app.run();
+	return 0;
+}
+
+*/
+#include <time.h>
+#include <iostream>
+#include "util/perlin_noise.h"
+
+int main(void)
+{
+	evn_util::PerlinNoise perlin_noise(16, time(0));
+
+	std::cout << "P3\n" << 256 << ' ' << 256 << "\n255\n";
+
+	
+
+	for (int i = 0; i < 256; i++) {
+		for (int j = 0; j < 256; j++) {
+			float x { (float)j };
+			float y { (float)i };
+			float val{ 0.0f };
+			float freq{ 1 };
+			float amp { 1 };
+			for (int k = 0; k < 8; k++) {
+				val += perlin_noise.perlin(x * freq / 16.0f, y * freq / 16.0f) * amp;
+				freq *= 2;
+				amp *= 0.5;
+			}
+			
+			int color = (int)(((val + 1.0f) * 0.5f) * 255);
+			std::cout << color << ' ' << color << ' ' << color << "\n";
+		}
+	}
+
 	return 0;
 }
---
+++ src/util/perlin_noise.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <stdint.h>
+#include <vector>
+#include <glm/glm.hpp>
+#include <random>
+
+namespace evn_util {
+	class PerlinNoise {
+	public:
+		PerlinNoise(uint16_t cell_dimensions, unsigned int seed);
+		~PerlinNoise();
+		float perlin(float x, float y);
+	private: // methods
+		void initCorners();
+		glm::vec2 randomGradient(int x, int y);
+		float dotGradient(int x0, int x1, float x, float y);
+		float ease(float a, float b, float c) const ;
+
+		inline float linear(float start, float end, float coef) { return coef * (end - start) + start; }
+		inline float poly(float coef) { return 3 * coef * coef - 2 * coef * coef * coef; }
+		inline float interp(float start, float end, float coef) { return linear(start, end, poly(coef)); }
+	private:
+		uint16_t m_dimensions;
+		uint32_t m_mesh_dimension;
+		std::vector<std::vector<glm::vec2>> m_corner_matrice;
+	};
+}
